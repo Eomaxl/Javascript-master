c54e110ec3d6cb9ccb4e67618ab8610814800553
Swikrity Mansingh <cardcaptor1990@gmail.com>
2021-02-28 20:51:48 +0530

    Applied string consistency and remvoed prototype dependency from human object

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* Generic function to select dom element using querySelector*/
 const selector = (val) => document.querySelector(val);
 
 /** @param {string} species
@@ -18,14 +19,16 @@ function DinoConstructor(species, weight, height, diet, where, when, fact) {
     this.fact = fact;
 }
 
+/* Fetch the data from the json file. */
 const getDinoData = async () => {
-    const JSONdata = await fetch('dino.json');
+    const JSONdata = await fetch(`dino.json`);
     const data = await JSONdata.json();
     return data.Dinos;
 }
 
 const dinos = [];
 
+/* Read the dino json file and create the dino objects using the constructor. */
 window.onload = async function() {
     const dinoArray = await getDinoData();
     /***@param {Dino} obj */
@@ -37,40 +40,39 @@ window.onload = async function() {
 
 const human = new Object();
 
-selector('.form-container #btn').addEventListener('click', () => {
+/* Function that would read the value from the form using click event. */
+selector(`.form-container #btn`).addEventListener(`click`, () => {
     (function getHumanData() {
-        human.fact  = selector('#name').value;
-        human.feet = selector('#feet').value;
-        human.inch = selector('#inches').value;
-        human.weight = selector('#weight').value;
-        human.diet = selector('#diet').value;
+        human.fact  = selector(`#name`).value;
+        human.feet = selector(`#feet`).value;
+        human.inch = selector(`#inches`).value;
+        human.weight = selector(`#weight`).value;
+        human.diet = selector(`#diet`).value;
         human.species = `human`;
     })();
 });
 
+/* Function to compare the height prototyped to the dinosaur object. */
 DinoConstructor.prototype.compareHeight = function() {
     const height = parseInt(human.feet)*12 + parseInt(human.inch);
     if( this.species !== `Pigeon`) {
-        if (this.species !== `human`){
             if( height < this.height) {
                 return `${this.species} is taller than ${human.fact}`;
             } else if (height > this.height) {
                 return `${this.species} is smaller than ${human.fact}`;
             } else {
                 return `${this.species} has the same height as of ${human.fact}`;
-            }
-        } else {
-            return human.fact;
-        }           
+            }            
     }
     else {
         return this.fact;
     }      
 }
 
+/* Function to compare the weight prototyped to the dinosaur object.*/
 DinoConstructor.prototype.compareWeight = function() {
     const humanWeight = parseInt(human.weight);
-    if( this.species !== `Pigeon`) {
+        if( this.species !== `Pigeon`) {
         if (humanWeight < this.weight + 20 && humanWeight > this.weight - 20){
             return `Close match! They average ${this.weight} pounds. But you probably wouldn't want to wrestle.`;
         } else if (humanWeight >= this.weight + 20){
@@ -80,13 +82,15 @@ DinoConstructor.prototype.compareWeight = function() {
             const weightDifference = Number.parseFloat(this.weight/human.weight).toPrecision(2);
             return `The ${this.species} is ${weightDifference} times larger than you. Don't get in the way.`;
         }
-    } else {
+    } 
+    else {
         return this.fact;
     }
 }
 
+/* Function to compare the diet prototyped to the dinosaur object.*/
 DinoConstructor.prototype.compareDiet = function(){
-    if (this.species !== `Pigeon` && this.species !== human.species) {
+    if (this.species !== `Pigeon`) {
         if (this.diet === human.diet) {
             return `${this.species} has the same diet as of ${human.fact}`;
         } else if (this.diet === `carnivor`) {
@@ -99,48 +103,55 @@ DinoConstructor.prototype.compareDiet = function(){
     }
 }
 
-selector('.form-container #btn').addEventListener('click', () => {
-    selector('#grid').style.display = "block";
+
+selector(`.form-container #btn`).addEventListener(`click`, () => {
+    selector(`#grid`).style.display = `block`;
 });
 
+/* Generates the dino tiles. */
 const generateTileForDinos = obj => {
-    const div = document.createElement("div");
-    const h3 = document.createElement("h3");
-    const p = document.createElement("p");
-    const img = document.createElement("img");
-
-    const tile = selector('main');
+    const div = document.createElement(`div`);
+    const h3 = document.createElement(`h3`);
+    const p = document.createElement(`p`);
+    const img = document.createElement(`img`);
+    const tile = selector(`main`);
     tile.append(div);
     div.append(h3);
     div.append(img);
     div.append(p);
-    (obj.species === `human`) ? h3.innerHTML = obj.fact : h3.innerHTML = obj.species;
-    p.innerHTML = ( () => {
-        let result = ``;
-        const rand = Math.floor(Math.random() * 7);
-
-        switch (rand) {
-            case 1:
-                result = obj.compareWeight();
-                break;
-            case 2:
-                result = obj.compareDiet();
-                break;
-            case 3:
-                result = obj.compareHeight();
-                break;
-            default:
-                result = obj.fact;
-                break;
-        }
-        return result;
-    })();
-    img.setAttribute("src", `./images/${obj.species.toLowerCase()}.png`)
-    div.setAttribute("class","grid-item");
+    if (obj.species === `human`){
+        h3.innerHTML = obj.fact;
+        p.innerHTML = `There are 7.5 billion people in this world.`;
+    } else {
+        h3.innerHTML = obj.species;
+        p.innerHTML = ( () => {
+            let result = ``;
+            const rand = Math.floor(Math.random() * 7);
+    
+            switch (rand) {
+                case 1:
+                    result = obj.compareWeight();
+                    break;
+                case 2:
+                    result = obj.compareDiet();
+                    break;
+                case 3:
+                    result = obj.compareHeight();
+                    break;
+                default:
+                    result = obj.fact;
+                    break;
+            }
+            return result;
+        })();
+    } 
+    img.setAttribute(`src`, `./images/${obj.species.toLowerCase()}.png`)
+    div.setAttribute(`class`,`grid-item`);
 }
 
-selector('.form-container #btn').addEventListener('click',() => {
-    selector('form').style.display = 'none';
+/* Click event upon submitting the form. */
+selector(`.form-container #btn`).addEventListener(`click`,() => {
+    selector(`form`).style.display = `none`;
     dinos.splice(4,0,human);
     dinos.forEach(ele => generateTileForDinos(ele));
 });
